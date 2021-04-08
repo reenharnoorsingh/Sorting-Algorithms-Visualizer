34b980cee884078c398a9151886ede2018fad12e
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-04-09 03:05:48 +0530

    base layout fixed

--- algorithm-visualizer.py
+++ algorithm-visualizer.py
@@ -4,13 +4,15 @@ from tkinter import ttk
 import random  # importing random to have a random set of values when visualized
 
 # tkinter layout
-root = Tk()
-root.title('Sorting Algorithms Visualizer')
-root.maxsize(900, 600)
-root.config(bg='black')
+tkinterRoot = Tk()
+tkinterRoot.title('Sorting Algorithms Visualizer')
+tkinterRoot.maxsize(900, 600)
+tkinterRoot.config(bg='black')
 
 # base layout
-user_frame = Frame(root, width=600, height=200, bg='grey')
-user_frame.grid(row=0, column=0, padx=10, pady=5)
+user_frame = Frame(tkinterRoot, width=600, height=200, bg='grey')   #frame is used to organize layout
+user_frame.grid(row=0, column=0, padx=10, pady=5)   #adds padding etc
 
-root.mainloop()
+user_canvas = Canvas(tkinterRoot, width=600, height=380, bg='white') #canvas is used to draw graphs or plots
+user_canvas.grid(row=1, column=0, padx=10, pady=5)
+tkinterRoot.mainloop()
