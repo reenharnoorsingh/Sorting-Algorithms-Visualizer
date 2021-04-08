5bccb03e1c69ddcdefe173ac87d4f05ba9104379
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-04-09 02:47:02 +0530

    layout of tkinter

--- algorithm-visualizer.py
+++ algorithm-visualizer.py
@@ -1,4 +1,15 @@
 #Sorting Algorithms Visualizer in Python
-from tkinter import *
-from tkinter import tkk
-import random
+from tkinter import * #importing tkinter
+from tkinter import ttk
+import random #importing random to have a random set of values when visualized
+
+#tkinter layout
+root = Tk()
+root.title('Sorting Algorithms Visualizer')
+root.maxsize(900,600)
+root.config(bg='black')
+
+
+
+
+root.mainloop()
