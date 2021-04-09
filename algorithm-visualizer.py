658e15fe1786e09cf4cdd147c4eda5346ac648b6
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-04-09 17:50:34 +0530

    user area defined

--- algorithm-visualizer.py
+++ algorithm-visualizer.py
@@ -9,15 +9,24 @@ tkinterRoot.title('Sorting Algorithms Visualizer')
 tkinterRoot.maxsize(900, 600)
 tkinterRoot.config(bg='black')
 
-# base layout
-user_frame = Frame(tkinterRoot, width=600, height=200, bg='grey')   #frame is used to organize layout
-user_frame.grid(row=0, column=0, padx=10, pady=5)   #adds padding etc
+# variables
+selectedAlgorithm = StringVar()
 
-user_canvas = Canvas(tkinterRoot, width=600, height=380, bg='white') #canvas is used to draw graphs or plots
+# base layout of grey and white area
+# frame is used to organize layout
+user_frame = Frame(tkinterRoot, width=600, height=200, bg='grey')
+user_frame.grid(row=0, column=0, padx=10, pady=5)  # adds padding etc
+
+# canvas is used to draw graphs or plots
+user_canvas = Canvas(tkinterRoot, width=600, height=380, bg='white')
 user_canvas.grid(row=1, column=0, padx=10, pady=5)
 
-#User Area
-#Label(user_frame, text="Algorithm: ")
+# User Area
 
-#runs the tkinter code and compiles it
+#Row[0]
+Label(user_frame, text="Algorithm: ", bg='grey').grid(row=0, column=0, padx=5, pady=5, sticky=W)
+visualizerMenu = ttk.Combobox(user_frame, textvariable=selectedAlgorithm, values=['Bubble Sort', 'Merge Sort'])
+visualizerMenu.grid(row=0, column=1, padx=5, pady=5)
+
+# runs the tkinter code and compiles it
 tkinterRoot.mainloop()
